968cb0070f39e3cab91e16b922b36c6561e1fc4f
Eluminae <eddie.barraco@idci-consulting.fr>
2017-08-11 17:29:54 +0200

    Little description on the main page.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,12 +9,6 @@ class DefaultController extends Controller
 {
     public function indexAction(Request $request)
     {
-        $Parsedown = new \Parsedown();
-
-        $body = $Parsedown->text('Hello _Parsedown_!');
-
-        return $this->render('default/index.html.twig', [
-            'body' => $body,
-        ]);
+        return $this->render('default/index.html.twig');
     }
 }
